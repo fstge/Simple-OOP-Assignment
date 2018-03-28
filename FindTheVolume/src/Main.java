23456ed466bb88e54674b1dccd0382e0c0a04de3
fstge <37878080+fstge@users.noreply.github.com>
2018-03-28 13:14:10 -0400

    added the whole file to the branch
    Maybe this will work

---
+++ FindTheVolume/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        RectangularPrism r1 = new RectangularPrism();
+        r1.getValues();
+        //r1.calculateValues();
+        r1.displayValues();
+        
+        RectangularPrism r2 = new RectangularPrism();
+        r2.getValues();
+        //r2.calculateValues();
+        r2.displayValues();
+        
+        r1.displayValues();
+    }
+    
+}
