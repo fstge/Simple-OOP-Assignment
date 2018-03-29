602568606d498a0bc290894df96c61cb3e67037d
fstge <37878080+fstge@users.noreply.github.com>
2018-03-29 13:58:07 -0400

    Add files via upload

--- FindTheVolume/src/Main.java
+++ FindTheVolume/src/Main.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         RectangularPrism r1 = new RectangularPrism();
         r1.getValues();
         //r1.calculateValues();
         r1.displayValues();
-        
-        Sphere s1 = new Sphere(5);
+        Scanner input = new Scanner (System.in);
+        System.out.println("What is the radius of the sphere");
+        Sphere s1 = new Sphere(input.nextDouble());
         s1.calculateValues();
         s1.displayValues();
         
